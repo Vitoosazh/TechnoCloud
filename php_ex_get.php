776cd43b0670fd4afa9890bee51ae9b27dc0bc44
unknown <vitoosazh@gmail.com>
2022-06-09 22:10:51 +0300

    first commit

---
+++ php_ex_get.php
@@ -0,0 +1,13 @@
+﻿<html>
+ <head>
+  <title>php example get</title>
+  <meta charset="utf-8"/>
+ </head>
+ <body>
+  <?php
+   $name = $_GET['name'];
+   $surname = $_GET['surname']; 
+   echo "Здравствуйте, ".$name." ".$surname."! Очень рады Вас видеть.";
+  ?>
+ </body>
+</html>
